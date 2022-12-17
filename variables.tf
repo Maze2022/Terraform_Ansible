d3d55d5822dca80c3abd49fe36f14f6d2d689a5a
Imaze Enabulele <ienabulele84+luit@gmail.com>
2022-12-17 05:38:14 +0000

    commit

--- variables.tf
+++ variables.tf
@@ -9,24 +9,29 @@ variable "access_ip" {
 }
 
 variable "main_instance_type" {
-    type = string
-    default = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
 
 variable "main_vol_size" {
-    type = number
-    default = 8
+  type    = number
+  default = 8
 }
 
 variable "main_instance_count" {
-    type = number
-    default = 1
+  type    = number
+  default = 1
 }
 
 variable "key_name" {
-    type = string
+  type = string
 }
 
 variable "public_key_path" {
-    type = string
+  type = string
+}
+
+variable "cloud9_ip" {
+  type    = string
+  default = "34.223.207.63/32"
 }
