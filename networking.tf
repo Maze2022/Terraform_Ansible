b97baccb211100a9f0e34998707a0c47df800537
Imaze Enabulele <ienabulele84+luit@gmail.com>
2023-01-06 01:03:18 +0000

    commit

--- networking.tf
+++ networking.tf
@@ -102,7 +102,7 @@ resource "aws_security_group_rule" "ingress_ssh" {
   from_port         = 22
   to_port           = 22
   protocol          = "-1"
-  cidr_blocks       = [var.access_ip, var.cloud9_ip]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.maze_sg.id
 }
 
