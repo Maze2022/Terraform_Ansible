a1bf9169f7fb4ecff487adfe14167bd869f47be2
Imaze <ienabulele84@gmail.com>
2023-01-14 20:37:44 +0000

    commit

--- networking.tf
+++ networking.tf
@@ -97,14 +97,14 @@ resource "aws_security_group_rule" "ingress_http" {
   security_group_id = aws_security_group.maze_sg.id
 }
 
-resource "aws_security_group_rule" "ingress_ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.maze_sg.id
-}
+# resource "aws_security_group_rule" "ingress_ssh" {
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = aws_security_group.maze_sg.id
+# }
 
 resource "aws_security_group_rule" "egress_all" {
   type              = "egress"
